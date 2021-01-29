5ef7e02c2b977a5ee062bfeca28a2597d3217aef
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-29 06:55:54 -0800

    add navbar

--- 2/js/app.js
+++ 2/js/app.js
@@ -1,8 +1,86 @@
   $(window).on('scroll load',function(){
-        if($(window).scrollTop()>795){
+        if($(window).scrollTop()>600){
             $('nav').addClass('nav-active');
         }else{
             $('nav').removeClass('nav-active');
         
         }
     })
+
+
+const toggleButton=document.querySelector('.toggle-button')
+const navLinks = document.getElementsByClassName("navLink")[0];
+const links = document.querySelectorAll('.navbarLink');
+const header = document.querySelector('.navbar')
+
+links.forEach((item) =>{
+    item.addEventListener("click", ()=>{
+        const el = document.getElementById(item.getAttribute("data-link"));
+        el.scrollIntoView({
+            behavior:"smooth",
+            block:"start"
+        })
+    })
+})
+
+$(document).ready(function(){
+    $('.toggle-button').click(function(){
+        $(".navLink").toggleClass("active");
+    })
+    $('.navbarLink').click(function(){
+        $(this).siblings().removeClass('active');
+        $(this).toggleClass('active');
+    })
+})
+
+//toggleButton.addEventListener('click',()=> {
+  //  navLinks.classList.toggle('active')
+//}) 
+//
+// window.addEventListener('scroll',event =>{
+//     let navigationLinks=document.querySelectorAll('.navbarLink');
+//     let fromTop=window.scrollY;
+
+//     navigationLinks.forEach(link =>{
+//         let section = document.querySelector(link.hash);
+
+//         if(
+//             section.offsetTop <= fromTop &&
+//             section.offsetTop + section.offsetHeight > fromTop 
+//         ){
+//             link.classList.add('active');
+//         } else{
+//             link.classList.remove('active');
+//         }
+//     })
+// })
+
+
+// //active navigation on scroll
+// for (const link of links) {
+//     link.addEventListener("click", clickHandler);
+//   }
+
+//   function clickHandler(e) {
+//     e.preventDefault();
+//     const href = this.getAttribute("href");
+//     const offsetTop = document.querySelector(href).offsetTop;
+
+//     scroll({
+//       top: offsetTop,
+//       behavior: "smooth"
+//     });
+//   }
+//   //
+//   window.onscroll = function(){
+//       var top = window.scrollY;
+//     console.log(top);
+//     if(top >= 200){
+//         header.classList.add('active');
+//      } else {
+//         header.classList.remove('active')
+//     }
+//     }
+    
+
+
