db0f8dc7b0fdc22ef6282f4a10dd1ffb78de702a
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-30 02:15:50 -0800

    navbar

--- 2/js/app.js
+++ 2/js/app.js
@@ -1,61 +1,60 @@
-  $(window).on('scroll load',function(){
-        if($(window).scrollTop()>600){
-            $('nav').addClass('nav-active');
-        }else{
-            $('nav').removeClass('nav-active');
-        
-        }
-    })
-
 
 const toggleButton=document.querySelector('.toggle-button')
-const navLinks = document.getElementsByClassName("navLink")[0];
+const navLinks = document.getElementsByClassName("navLink");
 const links = document.querySelectorAll('.navbarLink');
 const header = document.querySelector('.navbar')
 
 links.forEach((item) =>{
     item.addEventListener("click", ()=>{
-        const el = document.getElementById(item.getAttribute("data-link"));
+        let el = document.getElementById(item.getAttribute("data-link"));
         el.scrollIntoView({
             behavior:"smooth",
             block:"start"
         })
     })
 })
-
+//////////
 $(document).ready(function(){
     $('.toggle-button').click(function(){
         $(".navLink").toggleClass("active");
     })
-    $('.navbarLink').click(function(){
-        $(this).siblings().removeClass('active');
-        $(this).toggleClass('active');
-    })
+    $(".navbarItem").click(function () {
+      $(this).siblings().removeClass("active");
+      $(this).closes(".navbarItem").removeClass("active");
+    });
 })
 
 //toggleButton.addEventListener('click',()=> {
   //  navLinks.classList.toggle('active')
 //}) 
-//
-// window.addEventListener('scroll',event =>{
-//     let navigationLinks=document.querySelectorAll('.navbarLink');
-//     let fromTop=window.scrollY;
+// //
+window.addEventListener("scroll", (event) => {
+  let navigationLinks = document.querySelectorAll("nav ul li a");
+  let fromTop = window.scrollY;
 
-//     navigationLinks.forEach(link =>{
-//         let section = document.querySelector(link.hash);
+  navigationLinks.forEach((link) => {
+    if (link.hash != "" && link.hash != "#") {
+      let section = document.querySelector(link.hash);
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    }
+  });
+});
 
-//         if(
-//             section.offsetTop <= fromTop &&
-//             section.offsetTop + section.offsetHeight > fromTop 
-//         ){
-//             link.classList.add('active');
-//         } else{
-//             link.classList.remove('active');
-//         }
-//     })
-// })
-
-
+function activeLinkControl() {
+    $("nav .navbarItem a") .click(function(){
+        //remove active class from any of navbarItem
+        $(".navbarItem").removeClass('active');
+        //add active class to clicked item but at li not the anchor
+        $(this).closes(".navbarItem").addClass('active');
+    });
+}
 // //active navigation on scroll
 // for (const link of links) {
 //     link.addEventListener("click", clickHandler);
@@ -63,24 +62,16 @@ $(document).ready(function(){
 
 //   function clickHandler(e) {
 //     e.preventDefault();
-//     const href = this.getAttribute("href");
-//     const offsetTop = document.querySelector(href).offsetTop;
+//     const href = this.getAttribute("data-link");
+//     const offsetTop = document.querySelector(data-link).offsetTop;
 
 //     scroll({
 //       top: offsetTop,
 //       behavior: "smooth"
 //     });
 //   }
-//   //
-//   window.onscroll = function(){
-//       var top = window.scrollY;
-//     console.log(top);
-//     if(top >= 200){
-//         header.classList.add('active');
-//      } else {
-//         header.classList.remove('active')
-//     }
-//     }
+  //
+
     
 
 
